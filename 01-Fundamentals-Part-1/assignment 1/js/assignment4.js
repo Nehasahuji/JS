696fd279f86bf991acb2dd916ee28435964f5612
Neha Sahu <nehasahu9424@gmail.com>
2022-01-07 10:52:13 +0530

    Differnt ways to define function

---
+++ 01-Fundamentals-Part-1/assignment 1/js/assignment4.js
@@ -0,0 +1,14 @@
+ 
+//  function declaration
+ function callAge(birthYear){
+   return 2022-birthYear;
+}
+
+// function
+const callAge = function (birthYear){
+    return 2022-birthYear;
+}
+
+
+// arrow function
+const callAge = (birthyear) => 2022-birthyear;

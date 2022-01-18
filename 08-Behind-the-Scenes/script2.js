ab2b2c97c07f26ccd8d0cf5755de1d4ee5f618ba
Neha Sahu <nehasahu9424@gmail.com>
2022-01-18 23:20:40 +0530

    hoisting for varibales const/var/let

---
+++ 08-Behind-the-Scenes/script2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// we can not use variables before the are declared 
+console.log(salary);
+console.log(increment);
+console.log(name);
+
+var salary=200;
+let increment=5;
+const name = neha;
